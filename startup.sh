d1bb6501dff338391a6fe7297ccbc9bdd850d25b
Akhil <98938102+AKHIL-SI@users.noreply.github.com>
2022-04-25 02:17:16 +0530

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #    The TufaanBot
-#    Copyright (C) 2020 AKHIL-SI
+#    Copyright (C) 2022 AKHIL-SI
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
