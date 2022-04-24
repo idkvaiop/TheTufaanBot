a1a7230f87556cf235c293d1541c5b701299d45b
Akhil <98938102+AKHIL-SI@users.noreply.github.com>
2022-04-25 02:59:21 +0530

    Update __main__.py

--- userbot/__main__.py
+++ userbot/__main__.py
@@ -1,12 +1,12 @@
 import glob
-from telebot import bot
+from userbot import bot
 from sys import argv
 from telethon import TelegramClient
-from telebot.telebotConfig import Var
-from telebot.utils import load_module, start_mybot, load_pmbot
+from userbot.telebotConfig import Var
+from userbot.utils import load_module, start_mybot, load_pmbot
 from pathlib import Path
 import telethon.utils
-from telebot import CMD_HNDLR
+from userbot import CMD_HNDLR
 
 TELE = Var.PRIVATE_GROUP_ID
 BOTNAME = Var.TG_BOT_USER_NAME_BF_HER
@@ -21,7 +21,7 @@ async def add_bot(bot_token):
 
 async def startup_log_all_done():
     try:
-        await bot.send_message(TELE, f"**TeleBot has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of TeleBot**")
+        await bot.send_message(TELE, f"**Tufaan has been deployed.\nSend** `{CMD_HNDLR}alive` **to see if the bot is working.\n\nAdd** @{BOTNAME} **to this group and make it admin for enabling all the features of TufaanBot**")
     except BaseException:
         print("Either PRIVATE_GROUP_ID is wrong or you have left the group.")
 
@@ -38,13 +38,13 @@ else:
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
         print("Initialisation finished, no errors")
-        print("Starting Userbot")
+        print("Starting TufaanBot 😈")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Startup Completed")
     else:
         bot.start()
 
-path = 'telebot/plugins/*.py'
+path = 'userbot/plugins/*.py'
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -52,10 +52,10 @@ for name in files:
         shortname = path1.stem
         load_module(shortname.replace(".py", ""))
 
-print("TeleBot has been deployed! ")
+print("Your TufaanBot has been deployed 😈 ")
 
 print("Setting up TGBot")
-path = "telebot/plugins/mybot/*.py"
+path = "userbot/plugins/mybot/*.py"
 files = glob.glob(path)
 for name in files:
     with open(name) as f:
@@ -64,19 +64,30 @@ for name in files:
         start_mybot(shortname.replace(".py", ""))
 
 if LOAD_MYBOT == "True":
-    path = "telebot/plugins/mybot/pmbot/*.py"
+    path = "userbot/plugins/mybot/pmbot/*.py"
     files = glob.glob(path)
     for name in files:
         with open(name) as f:
             path1 = Path(f.name)
             shortname = path1.stem
             load_pmbot(shortname.replace(".py", ""))
-    print("TGBot set up completely!")
+    print("loading modules !")
 
-print("TGBot set up - Level - Basic")
-print("TeleBot has been fully deployed! Do Visit @TeleBotSupport")
+print("Starting TufaanBot 😈")
+print("TufaanBot is now alive. Check by doing (.alive/. ping) to see your Tufaan is working !@")
+
+
+# Join TufaanBot all channels after deploying 🤐😅
+    try:
+        await bot(JoinChannelRequest("@TheTufaanBot_Updates"))
+    except BaseException:
+        pass
+    try:
+        await bot(JoinChannelRequest("@TufaanBot_Support"))
+    except BaseException:
+         pass
+
 bot.loop.run_until_complete(startup_log_all_done())
-
 if len(argv) not in (1, 3, 4):
     bot.disconnect()
 else:
