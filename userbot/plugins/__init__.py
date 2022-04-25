2384834a02321ee52504ed8fe40299601afaeafd
Akhil <98938102+AKHIL-SI@users.noreply.github.com>
2022-04-25 17:12:26 +0530

    Update __init__.py

--- userbot/plugins/__init__.py
+++ userbot/plugins/__init__.py
@@ -1,24 +1,8 @@
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
-
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from telethon.tl.types import Channel
 
-from telebot import *
-from telebot import ALIVE_NAME, bot, telever
-from telebot.telebotConfig import Config, Var
+from userbot import *
+from userbot import ALIVE_NAME, bot, telever
+from userbot.exampleconfig import Config, Var
 
 # stats
 if Var.PRIVATE_GROUP_ID:
@@ -46,18 +30,17 @@ if Var.PMSECURITY.lower() == "off":
 else:
     pm = "Enabled"
 
-TELEUSER = str(ALIVE_NAME) if ALIVE_NAME else "@TeleBotSupport"
+ТυƒααηUser= str(ALIVE_NAME) if ALIVE_NAME else "@TufaanBot_Support"
 
-tele = f"TeleBot Version: {telever}\n"
-tele += f"Log Group: {log}\n"
-tele += f"Assistant Bot: {bots}\n"
-tele += f"Lydia: {lyd}\n"
-tele += f"Sudo: {sudo}\n"
-tele += f"PMSecurity: {pm}\n"
-tele += f"\nVisit @TeleBotSupport for assistance.\n"
-telestats = f"{tele}"
+tufaan = f"Tufaan Version: {telever}\n"
+tufaan += f"Log Group: {log}\n"
+tufaan += f"Assistant Bot: {bots}\n"
+tufaan += f"Sudo: {sudo}\n"
+tufaan += f"PMSecurity: {pm}\n"
+tufaan += f"\nVisit @TufaanBot_Support for assistance.\n"
+tufaanstats = f"{tufaan}"
 
-TELE_NAME = bot.me.first_name
+TUFAAN_NAME = bot.me.first_name
 OWNER_ID = bot.me.id
 
 # count total number of groups
